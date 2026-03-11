fc2edc73d57440c0766afa46ce7f011872c858bb
MehaThavamani09 <100196935@atu.ie>
2026-03-11 19:19:47 +0000

    Week1

---
+++ Week1.R
@@ -0,0 +1,64 @@
+x<-c(3,12,7)
+y<-c(4,9,11)
+z<-(x+y)
+z
+(x+2)
+x
+(y+50)
+y
+sum(x)
+mean(y)
+
+w<-c(2,7,NA,20)
+sum(w)
+sum(w, na.rm = TRUE)
+v <- sum(w, na.rm = TRUE)
+v
+help(sum)
+typeof(v)
+
+#greater than operator
+
+outer(x, y, ">")
+
+#append
+x <- append(x, 12)
+x
+
+
+(assign('a',c(10,150,30,45,20.3)))
+(a<-c(10,150,30,45,20.3))
+(a=c(10,150,30,45,20.3))
+(c(10,150,30,45,20.3)->x)
+
+(x<-c(20.5,39,45.9,7))
+(x+1)
+(x*5)
+(1/x)
+y<-x+5
+y
+(Z<-c(x,0,x))
+
+
+p<-c(1,2,3,4)
+q<-c(2,4,6,8)
+
+p<-c(1,2,3,4,6,8)
+q<-c(2,4,6,8)
+
+p<-c(1,2)
+q<-c(2,4,6,8)
+(p+q)
+(p-q)
+(p*q)
+(p/q)
+
+
+install.packages("swirl")
+library(swirl)
+swirl()
+
+
+
+
+
